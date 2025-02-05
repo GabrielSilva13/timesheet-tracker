e158facd37758260609bf670d4dbe2d6d5512e73
GabrielSilva13 <jeovabolado67@gmail.com>
2025-02-05 07:57:38 -0300

    adding reset timer button

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -6,9 +6,10 @@ import { useTimerStore } from "@/store/timer-store";
 import { cn } from "@/lib/utils";
 import Report from "./report";
 import DailyLog from "./daily-log";
+import { Button } from "./ui/button";
 
 export default function Timer() {
-  const { isRunning, startTimer, stopTimer, elapsedTime, startTime } = useTimerStore();
+  const { isRunning, startTimer, stopTimer, elapsedTime, startTime, resetTimer } = useTimerStore();
   const [timeDigits, setTimeDigits] = useState(["00", "00", "00"]);
 
   useEffect(() => {
@@ -77,6 +78,11 @@ export default function Timer() {
       </div>
 
       <div className="flex items-end justify-end w-full mt-auto gap-3">
+        <Button size="icon" variant="outline" onClick={resetTimer}>
+            <svg xmlns="XXXXXXXXXXXXXXXXXXXXXXXXXX" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+        </Button>
         <DailyLog />
         <Report />
       </div>
